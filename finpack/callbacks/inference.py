f469b1f95a63b00f81a3ebf165d2c2f0c46e0991
JesseYang <jesse.yang1985@gmail.com>
2017-07-06 23:01:41 +0800

    u

--- finpack/callbacks/inference.py
+++ finpack/callbacks/inference.py
@@ -3,8 +3,10 @@ import numpy
 # __all__ = ['NumericError', 'ClassificationError', 'BinaryClassificationStats']
 __all__ = ['NumericError']
 
+class Inferencer:
+	pass
 
-class NumericError:
+class NumericError(Inferencer):
 	def __init__(self, var_name, inf_name=None):
 		self.var_name = var_name
 		if inf_name == None:
